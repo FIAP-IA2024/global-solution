cfb17fcd2bc67244d33ccb2ec04e66bc549fc7a7
ribeirogab <ribeirogabx@gmail.com>
2024-11-20 15:46:01 -0300

    feat: add timestamp to mqtt event generation.

--- app/mqtt.py
+++ app/mqtt.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import json
 import random
 import time
+from datetime import datetime
 from database import save_event
 
 BROKER = "test.mosquitto.org"
@@ -13,22 +14,32 @@ PORT = 1883
 def generate_random_event():
     device_id = random.randint(1, 5)
     event_type = random.choice(["state-change", "sensor-reading", "energy-consumption"])
+    timestamp = datetime.utcnow().strftime(
+        "%Y-%m-%dT%H:%M:%SZ"
+    )  # Timestamp in UTC format
+
     if event_type == "state-change":
         return {
             "device_id": device_id,
-            "event_type": event_type,
+            "type": event_type,
+            "timestamp": timestamp,
             "value": random.choice(["on", "off"]),
+            "numeric_value": None,  # No numeric value for state-change
         }
     elif event_type == "sensor-reading":
         return {
             "device_id": device_id,
-            "event_type": event_type,
+            "type": event_type,
+            "timestamp": timestamp,
             "value": random.choice(["motion detected", "250 lux", "no motion"]),
+            "numeric_value": None,  # No numeric value for sensor readings
         }
     elif event_type == "energy-consumption":
         return {
             "device_id": device_id,
-            "event_type": event_type,
+            "type": event_type,
+            "timestamp": timestamp,
+            "value": None,  # No string value for energy consumption
             "numeric_value": round(random.uniform(0.01, 5.0), 3),
         }
 
@@ -45,6 +56,7 @@ def on_connect(client, userdata, flags, rc):
 def main():
     global CONNECTED
 
+    # Initialize the MQTT client
     client = mqtt.Client()
     client.on_connect = on_connect
     client.connect(BROKER, PORT)
@@ -66,13 +78,14 @@ def main():
             # Save event to database
             save_event(
                 device_id=event["device_id"],
-                event_type=event["event_type"],
+                event_type=event["type"],
                 value=event.get("value"),
                 numeric_value=event.get("numeric_value"),
             )
 
             print("Event saved to database.")
 
+            # Wait for 10 seconds before generating the next event
             time.sleep(10)
     except KeyboardInterrupt:
         print("Exiting...")
